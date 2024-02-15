937eb62589c1a858898528b459f6386fef978ab6
sichengchen <csc0330@hotmail.com>
2024-02-14 22:36:53 -0500

    Init commit

---
+++ app/lookup.jsx
@@ -0,0 +1,94 @@
+'use client'
+
+import {useState} from "react";
+
+const Lookup = ({data}) => {
+    const [prefId, setPrefId] = useState("dj")
+
+    const Region = ({prefId}) => {
+        if (prefId === "default") {
+            return <></>
+        } else {
+            const region = data.regions.find(r => r.id === prefId)
+            return <div className="mt-4 p-4 bg-white dark:bg-gray-900 max-w-4xl mx-auto sm:rounded-lg">
+                <div className="px-4 py-5 sm:px-6">
+                    <h3 className="text-lg leading-6 font-medium text-gray-900 dark:text-white">{region.name} {region["name-latin"]}</h3>
+                    <p className="mt-1 max-w-2xl text-sm text-gray-500 dark:text-gray-300">详细信息</p>
+                </div>
+                <div className="dark:text-white">
+                    <dl>
+                        <div className="bg-gray-50 dark:bg-gray-800 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium">中文名</dt>
+                            <dd className="mt-1 text-sm sm:mt-0 sm:col-span-2">{region.name}</dd>
+                        </div>
+                        <div className="bg-white dark:bg-gray-900 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium">拉丁名</dt>
+                            <dd className="mt-1 text-sm sm:mt-0 sm:col-span-2">{region["name-latin"]}</dd>
+                        </div>
+                        <div className="bg-gray-50 dark:bg-gray-800 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium">区号</dt>
+                            <dd className="mt-1 text-sm sm:mt-0 sm:col-span-2">{region["region-code"]}</dd>
+                        </div>
+                        <div className="bg-white dark:bg-gray-900 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium">简称</dt>
+                            <dd className="mt-1 text-sm sm:mt-0 sm:col-span-2">{region["zh-abbreviation"]} / {region.id.toUpperCase()}</dd>
+                        </div>
+                        <div className="bg-gray-50 dark:bg-gray-800 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium">类别</dt>
+                            <dd className="mt-1 text-sm sm:mt-0 sm:col-span-2">
+                                {data.models[0].regions.find(r => r.id === region.type).names.length > 1 ? data.models[0].regions.find(r => r.id === region.type).names.reduce((a, b) => a.zh + " (" + a.en + ")" + " / " + b.zh + " (" + b.en + ")") : data.models[0].regions.find(r => r.id === region.type).names[0].zh + " (" + data.models[0].regions.find(r => r.id === region.type).names[0].en + ")"}
+                            </dd>
+                        </div>
+                        <div className="bg-white dark:bg-gray-900 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium">首府</dt>
+                            <dd className="mt-1 text-sm sm:mt-0 sm:col-span-2">{region.capital}</dd>
+                        </div>
+                        <div className="bg-gray-50 dark:bg-gray-800 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium">建立年份</dt>
+                            <dd className="mt-1 text-sm sm:mt-0 sm:col-span-2">{region.est}</dd>
+                        </div>
+                        <div className="bg-white dark:bg-gray-900 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium">传送点域名</dt>
+                            <dd className="mt-1 text-sm sm:mt-0 sm:col-span-2">{region.id}.</dd>
+                        </div>
+                        <div className="bg-gray-50 dark:bg-gray-800 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium">
+                                {data.models[0].regions.find(r => r.id === region.type).governor.zh}
+                            </dt>
+                            <dd className="mt-1 text-sm sm:mt-0 sm:col-span-2">{region.governor}</dd>
+                        </div>
+                        <div className="bg-white dark:bg-gray-900 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium">
+                                副{data.models[0].regions.find(r => r.id === region.type).governor.zh}
+                            </dt>
+                            <dd className="mt-1 text-sm sm:mt-0 sm:col-span-2">{region["vice-governor"]}</dd>
+                        </div>
+                        <div className="bg-gray-50 dark:bg-gray-800 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium">信息更新于</dt>
+                            <dd className="mt-1 text-sm sm:mt-0 sm:col-span-2">{new Date(region["last-updated"]).toLocaleDateString()}</dd>
+                        </div>
+                    </dl>
+                </div>
+            </div>
+        }
+    }
+
+    return <div className="pb-4 flex-grow">
+        <div className="mt-4 p-4 bg-white dark:bg-gray-900 max-w-4xl mx-auto sm:rounded-lg">
+            <h4 className="text-2xl text-center mb-2 dark:text-white">查询区划信息</h4>
+            <p className="my-2 text-center text-sm text-gray-500 dark:text-gray-300">注：暂不支持查询二、三级区划。</p>
+            <select className="form-select block w-full mb-1 rounded-md p-2 border dark:bg-gray-800 dark:text-white dark:border-0" value={prefId}
+                    onChange={e => setPrefId(e.target.value)}>
+                {
+                    data.regions.map(r => <option value={r.id} key={r.id}>
+                        {r["region-code"]} {r.name}
+                    </option>)
+                }
+            </select>
+        </div>
+
+        <Region prefId={prefId}/>
+    </div>
+}
+
+export default Lookup
